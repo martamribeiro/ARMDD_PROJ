58d1b28a9075659fd6f6e2c8d60052cc040da7ed
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2024-12-30 12:56:11 +0000

    Removed unecessary file.

--- Scripts/StagingArea/CreateStagingAreaWithUkTables.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE ARMDD_PROJ_StagingArea   
-ON (FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\OperationalDB_UK.mdf'),  
-	(FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\OperationalDB_UK.ldf')  
-FOR ATTACH;
