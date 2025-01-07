107b2cc7c4ef9f23c70994b959f77cec71b2367f
Érica Lopes <ericamlopes21@hotmail.com>
2025-01-07 22:12:06 +0000

    added fact order update

--- ARMDD_PROJ_IntegrationServicesProject/SQL Scripts/Data Mart/CreateFactOrder.sql
+++ ARMDD_PROJ_IntegrationServicesProject/SQL Scripts/Data Mart/CreateFactOrder.sql
@@ -3,10 +3,10 @@ BEGIN
     CREATE TABLE [dbo].[FactOrder]
     (
         [orderID] INT NOT NULL,
-        [orderDateKey] INT NULL,
-        [productKey] INT NULL,
-        [customerKey] INT NULL,
-        [employeeKey] INT NULL,
+        [orderDateKey] INT NOT NULL,
+        [productKey] INT NOT NULL,
+        [customerKey] INT NOT NULL,
+        [employeeKey] INT NOT NULL,
         [shipDateKey] INT NULL,
         [requiredDatKey] INT NULL,
         [shipToKey] INT NULL,
@@ -19,14 +19,15 @@ BEGIN
         [discount] DECIMAL(19, 4) NULL,
         [discountUSD] DECIMAL(19, 4) NULL,
         [freight] DECIMAL(19, 4) NULL,
-        [freightUSD] DECIMAL(19, 4) NULL
+        [freightUSD] DECIMAL(19, 4) NULL,
+        PRIMARY KEY (orderDateKey, productKey, customerKey, employeeKey)
     )
 
-    -- CREATE NONCLUSTERED INDEX [NonClusteredIndex-FactOrder] ON [dbo].[FactOrder]
-    -- (
-    --     [orderDate] ASC,
-    --     [productKey] ASC,
-    --     [customerKey] ASC,
-    --     [employeeKey] ASC
-    -- )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+    CREATE NONCLUSTERED INDEX [NonClusteredIndex-FactOrder] ON [dbo].[FactOrder]
+    (
+        [orderDateKey] ASC,
+        [productKey] ASC,
+        [customerKey] ASC,
+        [employeeKey] ASC
+    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 END
